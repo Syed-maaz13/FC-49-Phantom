fadbe739ce45a8937fce9e618da949d4ed616278
Tejas <2001tejasjoshi@gmail.com>
2022-06-11 01:41:11 +0530

    Added bootstrap alert

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const submitbtn = document.getElementById('submit'),
   select1 = document.getElementById('exampleSelect1'),
-  select2 = document.getElementById('exampleSelect2');
+  select2 = document.getElementById('exampleSelect2'),
+  alertPlaceholder = document.getElementById('alertPlaceholder');
 
+// Convenient function to POST any URL using the fetch API
 async function postData(url = '', data = {}) {
   const response = await fetch(url, {
     method: 'POST',
@@ -19,6 +21,7 @@ async function postData(url = '', data = {}) {
 }
 const SubmitHandler = async (e) => {
   e.preventDefault();
+  // Validation
   if (
     select1.value === null ||
     select2.value === null ||
@@ -26,21 +29,43 @@ const SubmitHandler = async (e) => {
     select1.value == 'Select' ||
     select2.value == 'Select'
   ) {
-    console.log('Error');
+    // Creates and destroys a bootstrap alert
+    alert(
+      'Invalid inputs. Make sure source and destination are selected and are different.',
+      'danger'
+    );
+    setTimeout(() => {
+      const alert = bootstrap.Alert.getOrCreateInstance('#myAlert');
+      alert.close();
+    }, '3000');
   } else {
     const values = {
       source: select1.value,
       dest: select2.value,
       user: 'user',
     };
+    // Send to backend
     postData('http://localhost:3000', values)
       .then((data) => {
+        // Returns Price and QR Code
         console.log(data);
       })
       .catch((error) => {
         console.log('Error: ' + error);
       });
   }
+};
+// Bootstrap function to add an alert with message and type of alert
+const alert = (message, type) => {
+  const wrapper = document.createElement('div');
+  wrapper.innerHTML = [
+    `<div class="alert alert-${type} alert-dismissible" role="alert" id="myAlert">`,
+    `   <div>${message}</div>`,
+    '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+    '</div>',
+  ].join('');
+
+  alertPlaceholder.append(wrapper);
 };
 
 submitbtn.addEventListener('click', SubmitHandler);
